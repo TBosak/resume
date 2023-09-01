283cb3d8327dfb43d3310b697910d99de71d783b
Tim B <41713011+TBosak@users.noreply.github.com>
2023-09-01 12:39:57 -0500

    small changes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { PersistenceService } from 'src/services/persistence.service';
+import KeenSlder from 'keen-slider'
+import { Observable, concatMap, delay, from, of, repeat, tap } from 'rxjs';
 
 @Component({
   selector: 'app-about',
@@ -8,9 +10,19 @@ import { PersistenceService } from 'src/services/persistence.service';
 })
 export class AboutComponent implements OnInit {
   title: string = 'About';
+  images!: Observable<any>;
   constructor(private persistence: PersistenceService) { }
 
   ngOnInit() {
+    this.images = from([
+      'assets/slideshow/1.jpg',
+      'assets/slideshow/2.jpg',
+      'assets/slideshow/3.jpg',
+      'assets/slideshow/4.jpg',
+      'assets/slideshow/5.jpg',
+    ]).pipe(
+      concatMap(img => of(img).pipe(delay(2500))),
+      repeat());
   }
 
 }
